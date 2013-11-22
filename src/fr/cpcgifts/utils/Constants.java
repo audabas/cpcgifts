768d4b74e1be783f55a58393557b5e0d74db5a65
Bastien Auda <bastien26990@yahoo.fr>
2013-11-22 16:38:54 +0100

    Fix : faute de frappe.

--- src/fr/cpcgifts/utils/Constants.java
+++ src/fr/cpcgifts/utils/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
 	
 	public static final String TOPIC_DEV = "http://forum.canardpc.com/threads/79082-CPC-Gifts-Le-topic-des-d%C3%A9veloppeurs-g%C3%A9n%C3%A9reux-%28ou-pas%29";
 	
-	public static final String DEFAULT_RULES = "####Conditions d'inscritption :  \n\n" +
+	public static final String DEFAULT_RULES = "####Conditions d'inscription :  \n\n" +
 			"* Au moins un cadeau pour les canards sur Steamgifts ou CPC Gifts  \n" +
 			"* Au moins 25 messages postés sur le forum";
 	
